adaea7ecd9fe7293c192c44aa391d7f14f6c363d
Fabrizio Romanelli <fabrizio.romanelli@iit.it>
2018-12-18 15:00:05 +0100

    Adding gallery behavior for photograhpy

--- js/main.js
+++ js/main.js
@@ -286,7 +286,37 @@
 		})
 	};
 
-	// Document on load.
+    // Modal image function definition
+    $(function () {
+        "use strict";
+  
+        // Get the modal
+        var modal = document.getElementById('myModal');
+    
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var modalImg = document.getElementById("modalImg");
+        var captionText = document.getElementById("caption");
+
+        $(".blog-img img").click(function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        });
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+    
+        // When the user clicks on the opened image, close the modal
+        modalImg.onclick = function() {
+          modal.style.display = "none";
+        }
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { 
+        modal.style.display = "none";
+        }
+      });
+
+    // Document on load.
 	$(function(){
 		fullHeight();
 		counter();
